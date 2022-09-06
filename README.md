30d0fe431dd4708d374efe2b9bca372a038c2084
duo <hypersonolabs@gmail.com>
2022-09-06 18:32:04 +0900

    add create item route and route for retrieving all of items

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 2. running application
 
 - cd src && uvicorn main:app --reload
+
+3. check API docs
+
+- goto http://localhost:8000/docs
--- src/main.py
+++ src/main.py
@@ -1,9 +1,31 @@
 from typing import Union
 from fastapi import FastAPI
+from pydantic import BaseModel
 
+# init app
 app = FastAPI()
+
+# init db
+db = []
+
+
+class Item(BaseModel):
+    title: str
+    description: Union[str, None] = None
+    body: Union[str, None] = None
 
 
 @app.get("/")
 def index():
     return {"msg": "hello world for fastapi"}
+
+
+@app.post("/create-item")
+def create(item: Item):
+    db.append(item)
+    return item
+
+
+@app.get("/items")
+def read_item():
+    return db
