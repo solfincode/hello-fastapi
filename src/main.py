42d9a09b6b01f34b473e244f32ea7a9108a9a729
duo <hypersonolabs@gmail.com>
2022-09-07 17:34:24 +0900

    add delete route for item and select item using id in data

--- src/main.py
+++ src/main.py
@@ -33,6 +33,19 @@ async def read_item():
 
 
 @app.get("/items/{id}")
-async def get_item(id):
-    result = db[int(id)]
-    return result
+async def get_item(id: int):
+    selected = []
+    for el in db:
+        if (id == el.id):
+            selected.append(el)
+    return selected
+
+
+@app.delete("/items/{id}")
+async def delete_item(id: int):
+    deleted = []
+    for el in db:
+        if (id == el.id):
+            deleted.append(el)
+            del db[id]
+    return {"deleted": deleted}
